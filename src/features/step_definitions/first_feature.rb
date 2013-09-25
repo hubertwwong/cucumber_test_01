ce30d60eb8567447e5e8bdda3d0aa256066ed731
Hubert Wong <hubertwwong@gmail.com>
2013-09-25 00:05:22 -0700

    not sure if the step definitions are loading.

---
+++ src/features/step_definitions/first_feature.rb
@@ -0,0 +1,16 @@
+#encoding: utf-8
+Given /^a Cucumber that is (\d+) cm long$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+ 
+When /^I cut it in havles$/ do
+  pending # express the regexp above with the code you wish you had
+end
+ 
+Then /^I have two Cucumbers$/ do
+  pending # express the regexp above with the code you wish you had
+end
+ 
+Then /^both are (\d+) cm long$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
